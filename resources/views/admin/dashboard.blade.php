4c718b8b4a50685d1db06dc78307c63663f66ca9
J. Chris Tyler <jchristyler@gmail.com>
2021-03-18 00:28:15 -0500

    Add word validity to dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,7 @@
         <x-title />
         <section class="prose lg:prose-xl max-w-prose mx-auto">
             <livewire:datatable model="App\Models\Game" include="players.name,created_at,players.active_at,ended_at" />
-            <livewire:datatable model="App\Models\Word" include="word" per-page="50"/>
+            <livewire:datatable model="App\Models\Word" include="word,valid" per-page="50"/>
         </section>
     </x-main>
 </x-layout>
